16f93bb353fabf6de1c9896d0d328ee3fb45628a
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2025-09-17 19:48:26 +0530

    4 ms (99.48%) 54.8 MB (95.83%) Medium

--- LeetCode/Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ LeetCode/Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,16 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int n = prices.length;
-        int[][] dp = new int[n][2];
+        int hold = -prices[0];
+        int notHold = 0;
 
-        dp[0][0] = -prices[0]; // holding
-        dp[0][1] = 0;          // not holding
-
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] - prices[i]);
-            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + prices[i] - fee);
+        for (int i = 1; i < prices.length; i++) {
+            hold = Math.max(hold, notHold - prices[i]);
+            notHold = Math.max(notHold, hold + prices[i] - fee);
         }
-
-        return dp[n-1][1];
+        return notHold;
     }
 }
