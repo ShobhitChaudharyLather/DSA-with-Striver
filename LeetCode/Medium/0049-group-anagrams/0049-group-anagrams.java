11cc4ca4d346282518fcf781fc19b5e8ef1f8eb7
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2026-03-30 12:13:23 +0530

    Time: 7 ms (75.17%), Space: 49.5 MB (89.14%) - LeetHub

---
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String sort(String s){
+        char[] ch = s.toCharArray();
+        Arrays.sort(ch);
+        String sorted = new String(ch);
+        return sorted;
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map =new HashMap<>();
+        List<List<String>> res = new ArrayList<>();
+        for(int i = 0; i < strs.length; i++){
+            String key = sort(strs[i]);
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
+            }
+            map.get(key).add(strs[i]);
+        }
+        for(Map.Entry<String,List<String>> entry : map.entrySet()){
+            res.add(entry.getValue());
+        }
+        return res;
+    }
+}
