ce424ad3a60c530afaed71a58eccf360ada42102
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2026-04-09 12:14:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0704-binary-search/0704-binary-search.java
+++ LeetCode/Easy/0704-binary-search/0704-binary-search.java
@@ -8,10 +8,10 @@ class Solution {
                 return mid;
             }
             else if(target > nums[mid]){
-                l++;
+                l = mid + 1;
             }
             else{
-                r--;
+                r = mid - 1;
             }
         }
         return -1;
