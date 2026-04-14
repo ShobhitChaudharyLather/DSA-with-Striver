7b3fd3788f96f2969661456bc739f6514acb5cfe
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2026-04-14 10:07:22 +0530

    Time: 0 ms (100%), Space: 45.5 MB (80.48%) - LeetHub

--- LeetCode/Easy/0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ LeetCode/Easy/0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,15 +15,23 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        return dfsHeight(root) != -1;
+        return height(root) != -1;
     }
-    public int dfsHeight(TreeNode root){
-        if(root == null) return 0;
-        int left = dfsHeight(root.left);
-        if(left == -1) return -1;
-        int right = dfsHeight(root.right);
-        if(right == -1) return -1;
-        if(Math.abs(left - right) > 1) return -1;
-        return 1 + Math.max(left,right);
+    public int height(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = height(root.left);
+        if(left == -1){
+            return -1;
+        }
+        int right = height(root.right);
+        if(right == -1){
+            return -1;
+        }
+        if(Math.abs(left - right) > 1){
+            return -1;
+        }
+        return 1 + Math.max(left, right);
     }
 }
