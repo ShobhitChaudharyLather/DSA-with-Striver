47f25afade3f217ad62098eced76ed5cb1be4fbf
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2026-04-20 20:04:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
@@ -3,6 +3,7 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         int count = 0;
+        //no. of times dfs called = no. of islands
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == '1'){
