4a7277bdc699449a7b8713483a91a7ccf793bbdd
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2026-04-24 10:27:14 +0530

    Time: 2 ms (90.34%), Space: 44.1 MB (57.92%) - LeetHub

--- LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.java
+++ LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,35 +1,43 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int rows=grid.length;
-        int cols=grid[0].length;
-        Queue <int[]> q= new LinkedList<>();
-        int fresh=0;
-        for(int r=0;r<rows;r++){
-            for(int c=0;c<cols;c++){
-                if(grid[r][c]==2){
-                    q.offer(new int[]{r,c,0});
+        int row = grid.length;
+        int col = grid[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+
+        int fresh = 0;
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == 2){
+                    q.offer(new int[]{i, j, 0});
                 }
-                else if(grid[r][c]==1){
+                else if(grid[i][j] == 1){
                     fresh++;
                 }
             }
         }
-        int[][] direction={{-1,0},{1,0},{0,-1},{0,1}};
-        int time=0;
+
+        int time = 0;
+        int[][] directions = {{-1,0}, {0,1}, {1,0}, {0,-1}};
         while(!q.isEmpty()){
-            int[] curr= q.poll();
-            int r=curr[0], c=curr[1], t=curr[2];
-            time=Math.max(time,t);
-            for(int[]dir:direction){
-                int newR= r + dir[0];
-                int newC= c + dir[1];
-                if(newR>=0 && newR<rows && newC>=0 && newC<cols && grid[newR][newC]==1){
-                    grid[newR][newC]=2;
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+            int t = curr[2];
+            time = Math.max(time, t);
+            for(int[] d : directions){
+                int newR = r + d[0];
+                int newC = c + d[1];
+
+                if(newR >=0 && newR < row && newC >=0 && newC < col && grid[newR][newC] == 1){
+                    grid[newR][newC] = 2;
                     fresh--;
-                    q.offer(new int[]{newR,newC,t+1});
+                    q.offer(new int[]{newR, newC, t + 1});
                 }
             }
+            
         }
-        return fresh==0 ? time : -1;
+        return fresh == 0 ? time : -1;
     }
 }
